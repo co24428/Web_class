99ffe9c6b4be62a15cdea27d897da4cf3fc20f16
IanKim <ihwan1236@gmail.com>
2024-07-18 18:41:05 -0700

    Day 3 - 1

---
+++ Week_1/JS_practice/src/script.js
@@ -0,0 +1,18 @@
+const message = 'Hello world' // Try edit me
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+
+myVariable = "Bob";
+// Log to console
+console.log(myVariable)
+
+
+const myList = [1,2,3,4,5];
+
+myList.forEach((element) => console.log(element))
+
+
+for (let i=0; i<5;i++) {
+  console.log(i)
+}
