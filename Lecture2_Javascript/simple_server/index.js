77471a63cf0b3be0f74e45b37335ff122ce89445
IanKim <ihwan1236@gmail.com>
2024-09-10 17:44:16 -0700

    reorganizing directory

---
+++ Lecture2_Javascript/simple_server/index.js
@@ -0,0 +1,57 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
+const { mimeType, getSanitizedPathname } = require("./lib.js");
+
+const server = http.createServer((req, res) => {
+  // function to handle http request
+  // e.g. http://localhost:8085/grid.html?username=apple
+  let safePathname = getSanitizedPathname(req);
+  const queryObject = url.parse(req.url, true).query;
+
+  console.log(queryObject?.username);
+
+  fs.exists(safePathname, function (exist) {
+    if (!exist) {
+      // if the file is not found, return 404
+      res.statusCode = 404;
+      res.end(`File ${safePathname} not found!`);
+      return;
+    }
+
+    // if is a directory, then look for index.html
+    if (fs.statSync(safePathname).isDirectory()) {
+      safePathname += "/index.html";
+    }
+
+    // read file from file system
+    fs.readFile(safePathname, function (err, data) {
+      if (err) {
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
+      } else {
+        // Read file extention, e.g. .js, .html, etc.
+        const ext = path.parse(safePathname).ext;
+        // Set Content-Type and send data
+        res.setHeader("Content-Type", mimeType[ext] || "text/plain");
+
+        const uname = queryObject?.username;
+
+        if (uname) {
+          let repldata = data.toString().replace("%UNAME%", uname);
+          res.end(repldata);
+        } else {
+          let repldata = data.toString().replace("%UNAME%", "");
+          res.end(repldata);
+        }
+      }
+    });
+  });
+});
+
+server.listen(8085, () => {
+  console.log("Server running at http://localhost:8085");
+});
+
+console.log("log from index.js");
