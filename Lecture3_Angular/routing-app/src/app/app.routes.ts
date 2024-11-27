b19a160b3106481426c4542d7dbdf1f7b3f72327
IanKim <ihwan1236@gmail.com>
2024-11-26 16:30:49 -0800

    default route setup: home, login

--- Lecture3_Angular/routing-app/src/app/app.routes.ts
+++ Lecture3_Angular/routing-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent }
+];
