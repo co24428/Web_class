0e3063b1be101538e0be895da83814ca08d6294c
IanKim <ihwan1236@gmail.com>
2024-11-26 16:31:36 -0800

    rest of commit

--- Lecture3_Angular/form_example/src/app/components/reactive/group-form-control/group-form-control.component.ts
+++ Lecture3_Angular/form_example/src/app/components/reactive/group-form-control/group-form-control.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-group-form-control',
@@ -8,12 +10,46 @@ import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './group-form-control.component.html',
   styleUrl: './group-form-control.component.css'
 })
+
+
+
+
 export class GroupFormControlComponent implements OnInit {
-  profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
+  constructor (private fb: FormBuilder) {}
+  logErrors() {
+    console.log(this.profileForm.controls['firstName'].errors);
+    console.log(this.profileForm.controls['lastName'].errors);
+  }
+
+  // formBuilder is much more simpler
+  profileForm = this.fb.group({
+    firstName: ['', [Validators.required, Validators.minLength(4)]],
+    lastName: ['', [Validators.required, Validators.minLength(4)]],
+    address: this.fb.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        zip: ['']
+    })
   });
+
+  // profileForm = new FormGroup({
+  //   firstName: new FormControl(''),
+  //   lastName: new FormControl(''),
+  //   address: new FormGroup({
+  //       street: new FormControl(''),
+  //       city: new FormControl(''),
+  //       state: new FormControl(''),
+  //       zip: new FormControl('')
+  //   })
+  // });
+
   onSubmit() {console.warn(this.profileForm.value);}
-  constructor () {}
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Ariel',
+      address: { street: '123 W Hastings St'}
+    });
+  }
   ngOnInit(): void {}
 }
