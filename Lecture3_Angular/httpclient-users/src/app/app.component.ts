13dc5bde35c6f6a64e30249fa6b05af453106575
IanKim <ihwan1236@gmail.com>
2024-12-04 20:03:24 -0800

    initialization, pipe_example

--- Lecture3_Angular/httpclient-users/src/app/app.component.ts
+++ Lecture3_Angular/httpclient-users/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
