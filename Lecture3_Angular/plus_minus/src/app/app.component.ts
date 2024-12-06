5a664c5b45e6025cbaa8947c462a431195d848fd
IanKim <ihwan1236@gmail.com>
2024-12-05 18:35:21 -0800

    plusMinus using ngClass and ngStyle

--- Lecture3_Angular/plus_minus/src/app/app.component.ts
+++ Lecture3_Angular/plus_minus/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // for ngClass, ngStyle
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'plus_minus';
+  count: number = 0;
+
+  changeCount(num:number){
+    this.count += num;
+  }
 }
