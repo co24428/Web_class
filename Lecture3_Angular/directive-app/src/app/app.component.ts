20522a15e931242f787df122ec211f8d9b44ddff
IanKim <ihwan1236@gmail.com>
2024-12-06 12:29:13 -0800

    ngFor, ngIf, ngSwitch

--- Lecture3_Angular/directive-app/src/app/app.component.ts
+++ Lecture3_Angular/directive-app/src/app/app.component.ts
@@ -18,4 +18,15 @@ export class AppComponent {
   getCSSClasses(flag: string) {
     return flag == 'lightMode' ? 'one' : 'two'
   }
+
+  spends: any[] = [
+    { price: 15.70, place: 'Loblaws'},
+    { price: 11.20, place: 'HMart'},
+    { price: 3.99, place: 'Rexall'}
+  ]
+
+  isActive = true;
+  toggleImage(){
+    this.isActive = !this.isActive;
+  }
 }
