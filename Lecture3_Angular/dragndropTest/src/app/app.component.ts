358a730e9abc646571eb76ec71442f275e2cad8e
IanKim <ihwan1236@gmail.com>
2024-12-10 22:25:06 -0800

    dragndrop following the angular document

---
+++ Lecture3_Angular/dragndropTest/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CdkDropList, CdkDrag],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+
+})
+export class AppComponent {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+}
