f4c246d0d4957823df681ca7f9c8f7efe6263c62
IanKim <ihwan1236@gmail.com>
2024-12-30 03:31:43 -0800

    flatfinder start

---
+++ flatfinder/src/app/components/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  onLogin() {
+    console.log('Login attempt:', this.email, this.password)
+  }
+
+
+}
