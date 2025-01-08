4fdf6895f996a88904f63ade1ddb908a07592881
IanKim <ihwan1236@gmail.com>
2025-01-08 02:22:01 -0800

    login: Material, ing...

--- flatfinder/src/app/components/login/login.component.ts
+++ flatfinder/src/app/components/login/login.component.ts
@@ -1,16 +1,43 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import { MaterialModule } from '../../material/material.module';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {merge} from 'rxjs';
+
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [MaterialModule, 
+    FormsModule, 
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
+  
 })
 export class LoginComponent {
-  email: string = '';
-  password: string = '';
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(6)]); // Password must be at least 6 characters
+
+  errorMessage = signal('');
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
 
   onLogin() {
     console.log('Login attempt:', this.email, this.password)
---
+++ flatfinder/src/app/material/material.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from
+'@angular/material/progress-spinner';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+// MatNativeDateModule,,,
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule, MatListModule, MatMenuModule, MatTabsModule,
+    MatCardModule, MatFormFieldModule, MatInputModule, MatProgressSpinnerModule, DragDropModule, FormsModule, ReactiveFormsModule, 
+    // BrowserAnimationsModule
+  ],
+  exports: [
+    CommonModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatButtonModule, MatListModule, MatMenuModule, MatTabsModule,
+    MatCardModule, MatFormFieldModule, MatInputModule, MatProgressSpinnerModule, DragDropModule, FormsModule, ReactiveFormsModule, 
+    // BrowserAnimationsModule
+  ]
+})
+export class MaterialModule { }
