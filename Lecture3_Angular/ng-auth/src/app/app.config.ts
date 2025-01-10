b3e133d20f8f41cd4f218ef4e8ceb451e81359a7
IanKim <ihwan1236@gmail.com>
2025-01-10 14:03:26 -0800

    React: typescript

--- Lecture3_Angular/ng-auth/src/app/app.config.ts
+++ Lecture3_Angular/ng-auth/src/app/app.config.ts
@@ -9,13 +9,13 @@ import { CommonModule } from '@angular/common'; // Import CommonModule
 import { routes } from './app.routes';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5RcHDEPk8GtXEGARBK5QcWSJzL2AC5f4",
-  authDomain: "test-7b99b.firebaseapp.com",
-  projectId: "test-7b99b",
-  storageBucket: "test-7b99b.firebasestorage.app",
-  messagingSenderId: "1012144607473",
-  appId: "1:1012144607473:web:6fd44c4179112292d55950",
-  measurementId: "G-Q31RR8LHYH"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 export const appConfig: ApplicationConfig = {
